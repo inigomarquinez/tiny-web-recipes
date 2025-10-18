095c89eca6d12454ee41b0f80658c6769adf4320
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2025-10-18 13:40:10 +0200

    feat: configure site URL and base path in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://inigomarquinezprado.dev',
+    base: '/tiny-web-recipes',
     integrations: [starlight({
         title: 'Tiny Web Recipes',
         social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/inigomarquinez/tiny-web-recipes' }],
